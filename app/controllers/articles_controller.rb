6b5bfca4021534efa61133deea7f6fd0b3fcc8c2
vixa <vixa@bsenac.fr>
2019-04-21 11:23:07 -0400

    Add basic authentification

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
-    def new
+  
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  
+  def new
     end
 
     def create
