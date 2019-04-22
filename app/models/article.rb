dfa33a43be230aaf5d99ac7d8461b15eadf6981b
tpedrero <tony.pedrero@etu.u-bordeaux.fr>
2019-04-22 18:13:27 +0200

    add images

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
     has_many :comments, dependent: :destroy
+    has_one_attached :picture, dependent: :destroy
     validates :title, presence: true, length: { minimum: 5 }
     validates :text, presence:true, length: {maximum: 1000}
 
