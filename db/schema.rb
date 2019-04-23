9b88e16a661537661ef6d1e614799a6b7eeee73a
vixa <vixa@bsenac.fr>
2019-04-23 12:45:31 -0400

    Add user comments

--- db/schema.rb
+++ db/schema.rb
@@ -41,12 +41,13 @@ ActiveRecord::Schema.define(version: 2019_04_22_192523) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "commenter"
+    t.integer "user_id"
     t.text "body"
     t.integer "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
